4ecd134ccc1dc6cafa870dc75a0a66bdfae09b51
Orr Benyamini <orr.bob@hotmail.com>
2018-07-24 00:52:26 +0300

    Merge-sort

--- Java/utility-belt/src/main/java/algorithms/Sort.java
+++ Java/utility-belt/src/main/java/algorithms/Sort.java
@@ -1,44 +1,319 @@
 package algorithms;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class Sort {
 
-	//*******************************************************
-	//	Merge-Sort
-	//*******************************************************
+	// *******************************************************
+	// Merge-Sort
+	// *******************************************************
 
 	public static void mergeSort(int[] array) {
+		sort(array, 0, array.length - 1);
+	}
 
+	static void sort(int arr[], int l, int r) {
+		if (l < r) {
+			int m = (l + r) / 2;
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+			merge(arr, l, m, r);
+		}
+	}
+
+	private static void merge(int arr[], int l, int m, int r) {
+		int n1 = m - l + 1, n2 = r - m;
+		int[] left = Arrays.copyOfRange(arr, l, l + n1);
+		int[] right = Arrays.copyOfRange(arr, m + 1, m + 1 + n2);
+
+		int i = 0, j = 0, k = l;
+		while (i < n1 && j < n2) {
+			if (left[i] <= right[j]) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
 	}
 
 	public static void mergeSort(float[] array) {
+		sort(array, 0, array.length - 1);
+	}
 
+	static void sort(float arr[], int l, int r) {
+		if (l < r) {
+			int m = (l + r) / 2;
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+			merge(arr, l, m, r);
+		}
+	}
+
+	private static void merge(float arr[], int l, int m, int r) {
+		int n1 = m - l + 1, n2 = r - m;
+		float[] left = Arrays.copyOfRange(arr, l, l + n1);
+		float[] right = Arrays.copyOfRange(arr, m + 1, m + 1 + n2);
+
+		int i = 0, j = 0, k = l;
+		while (i < n1 && j < n2) {
+			if (left[i] <= right[j]) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
 	}
 
 	public static void mergeSort(double[] array) {
+		sort(array, 0, array.length - 1);
+	}
 
+	static void sort(double arr[], int l, int r) {
+		if (l < r) {
+			int m = (l + r) / 2;
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+			merge(arr, l, m, r);
+		}
+	}
+
+	private static void merge(double arr[], int l, int m, int r) {
+		int n1 = m - l + 1, n2 = r - m;
+		double[] left = Arrays.copyOfRange(arr, l, l + n1);
+		double[] right = Arrays.copyOfRange(arr, m + 1, m + 1 + n2);
+
+		int i = 0, j = 0, k = l;
+		while (i < n1 && j < n2) {
+			if (left[i] <= right[j]) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
 	}
 
 	public static void mergeSort(short[] array) {
+		sort(array, 0, array.length - 1);
+	}
 
+	static void sort(short arr[], int l, int r) {
+		if (l < r) {
+			int m = (l + r) / 2;
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+			merge(arr, l, m, r);
+		}
+	}
+
+	private static void merge(short arr[], int l, int m, int r) {
+		int n1 = m - l + 1, n2 = r - m;
+		short[] left = Arrays.copyOfRange(arr, l, l + n1);
+		short[] right = Arrays.copyOfRange(arr, m + 1, m + 1 + n2);
+
+		int i = 0, j = 0, k = l;
+		while (i < n1 && j < n2) {
+			if (left[i] <= right[j]) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
 	}
 
 	public static void mergeSort(long[] array) {
-
+		sort(array, 0, array.length - 1);
 	}
 
-	public static <T extends Comparable<?>> void mergeSort(T[] array) {
-
+	static void sort(long arr[], int l, int r) {
+		if (l < r) {
+			int m = (l + r) / 2;
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+			merge(arr, l, m, r);
+		}
 	}
 
+	private static void merge(long arr[], int l, int m, int r) {
+		int n1 = m - l + 1, n2 = r - m;
+		long[] left = Arrays.copyOfRange(arr, l, l + n1);
+		long[] right = Arrays.copyOfRange(arr, m + 1, m + 1 + n2);
+
+		int i = 0, j = 0, k = l;
+		while (i < n1 && j < n2) {
+			if (left[i] <= right[j]) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
+	}
+	
+	public static <T extends Comparable<T>> void mergeSort(T[] array) {
+		sort(array, 0, array.length - 1);
+	}
+
+	static <T extends Comparable<T>> void sort(T arr[], int l, int r) {
+		if (l < r) {
+			int m = (l + r) / 2;
+			sort(arr, l, m);
+			sort(arr, m + 1, r);
+			merge(arr, l, m, r);
+		}
+	}
+
+	private static <T extends Comparable<T>>  void merge(T arr[], int l, int m, int r) {
+		int n1 = m - l + 1, n2 = r - m;
+		T[] left = Arrays.copyOfRange(arr, l, l + n1);
+		T[] right = Arrays.copyOfRange(arr, m + 1, m + 1 + n2);
+
+		int i = 0, j = 0, k = l;
+		while (i < n1 && j < n2) {
+			if (left[i].compareTo(right[j]) <= 0) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
+	}
+	
 	public static <T> void mergeSort(T[] array, Comparator<T> comperator) {
-
+		sort(array, 0, array.length - 1, comperator);
 	}
 
-	//*******************************************************
-	//	Quick-Sort
-	//*******************************************************
+	static <T> void sort(T arr[], int l, int r, Comparator<T> comperator) {
+		if (l < r) {
+			int m = (l + r) / 2;
+			sort(arr, l, m, comperator);
+			sort(arr, m + 1, r, comperator);
+			merge(arr, l, m, r, comperator);
+		}
+	}
+
+	private static <T>  void merge(T arr[], int l, int m, int r, Comparator<T> comperator) {
+		int n1 = m - l + 1, n2 = r - m;
+		T[] left = Arrays.copyOfRange(arr, l, l + n1);
+		T[] right = Arrays.copyOfRange(arr, m + 1, m + 1 + n2);
+
+		int i = 0, j = 0, k = l;
+		while (i < n1 && j < n2) {
+			if (comperator.compare(left[i], right[j]) <= 0) {
+				arr[k] = left[i];
+				i++;
+			} else {
+				arr[k] = right[j];
+				j++;
+			}
+			k++;
+		}
+
+		while (i < n1) {
+			arr[k] = left[i];
+			i++;
+			k++;
+		}
+
+		while (j < n2) {
+			arr[k] = right[j];
+			j++;
+			k++;
+		}
+	}
+
+	
+	// *******************************************************
+	// Quick-Sort
+	// *******************************************************
 
 	public static void quickSort(int[] array) {
 
@@ -68,25 +343,27 @@ public class Sort {
 
 	}
 
-	//*******************************************************
-	//	Insertion-Sort
-	//*******************************************************
+	// *******************************************************
+	// Insertion-Sort
+	// *******************************************************
 
 	/**
-	 * Sorting an array of integers according to the <i>Insertion-Sort algorithm</i>
-	 * after calling this function the given array will be sorted in ascending order
+	 * Sorting an array of integers according to the <i>Insertion-Sort
+	 * algorithm</i> after calling this function the given array will be sorted
+	 * in ascending order
 	 * 
-	 * @param array - int array to be sorted
+	 * @param array
+	 *            - int array to be sorted
 	 */
 	public static void insertionSort(int[] array) {
-		if(array.length <= 1)
+		if (array.length <= 1)
 			return;
 		int size = array.length;
 		int key;
-		for(int i = 1; i < size; i++) {
+		for (int i = 1; i < size; i++) {
 			key = array[i];
 			int j = i - 1;
-			while(j>=0 && key < array[j]) {
+			while (j >= 0 && key < array[j]) {
 				array[j + 1] = array[j];
 				j -= 1;
 			}
@@ -95,20 +372,22 @@ public class Sort {
 	}
 
 	/**
-	 * Sorting an array of floats according to the <i>Insertion-Sort algorithm</i>
-	 * after calling this function the given array will be sorted in ascending order
+	 * Sorting an array of floats according to the <i>Insertion-Sort
+	 * algorithm</i> after calling this function the given array will be sorted
+	 * in ascending order
 	 * 
-	 * @param array - float array to be sorted
+	 * @param array
+	 *            - float array to be sorted
 	 */
 	public static void insertionSort(float[] array) {
-		if(array.length <= 1)
+		if (array.length <= 1)
 			return;
 		int size = array.length;
 		float key;
-		for(int i = 1; i < size; i++) {
+		for (int i = 1; i < size; i++) {
 			key = array[i];
 			int j = i - 1;
-			while(j>=0 && key < array[j]) {
+			while (j >= 0 && key < array[j]) {
 				array[j + 1] = array[j];
 				j -= 1;
 			}
@@ -117,20 +396,22 @@ public class Sort {
 	}
 
 	/**
-	 * Sorting an array of doubles according to the <i>Insertion-Sort algorithm</i>
-	 * after calling this function the given array will be sorted in ascending order
+	 * Sorting an array of doubles according to the <i>Insertion-Sort
+	 * algorithm</i> after calling this function the given array will be sorted
+	 * in ascending order
 	 * 
-	 * @param array - double array to be sorted
+	 * @param array
+	 *            - double array to be sorted
 	 */
 	public static void insertionSort(double[] array) {
-		if(array.length <= 1)
+		if (array.length <= 1)
 			return;
 		int size = array.length;
 		double key;
-		for(int i = 1; i < size; i++) {
+		for (int i = 1; i < size; i++) {
 			key = array[i];
 			int j = i - 1;
-			while(j>=0 && key < array[j]) {
+			while (j >= 0 && key < array[j]) {
 				array[j + 1] = array[j];
 				j -= 1;
 			}
@@ -139,20 +420,22 @@ public class Sort {
 	}
 
 	/**
-	 * Sorting an array of short according to the <i>Insertion-Sort algorithm</i>
-	 * after calling this function the given array will be sorted in ascending order
+	 * Sorting an array of short according to the <i>Insertion-Sort
+	 * algorithm</i> after calling this function the given array will be sorted
+	 * in ascending order
 	 * 
-	 * @param array - short array to be sorted
+	 * @param array
+	 *            - short array to be sorted
 	 */
 	public static void insertionSort(short[] array) {
-		if(array.length <= 1)
+		if (array.length <= 1)
 			return;
 		int size = array.length;
 		short key;
-		for(int i = 1; i < size; i++) {
+		for (int i = 1; i < size; i++) {
 			key = array[i];
 			int j = i - 1;
-			while(j>=0 && key < array[j]) {
+			while ((j >= 0) && (key < array[j])) {
 				array[j + 1] = array[j];
 				j -= 1;
 			}
@@ -161,20 +444,22 @@ public class Sort {
 	}
 
 	/**
-	 * Sorting an array of longs according to the <i>Insertion-Sort algorithm</i>
-	 * after calling this function the given array will be sorted in ascending order
+	 * Sorting an array of longs according to the <i>Insertion-Sort
+	 * algorithm</i> after calling this function the given array will be sorted
+	 * in ascending order
 	 * 
-	 * @param array - long array to be sorted
+	 * @param array
+	 *            - long array to be sorted
 	 */
 	public static void insertionSort(long[] array) {
-		if(array.length <= 1)
+		if (array.length <= 1)
 			return;
 		int size = array.length;
 		long key;
-		for(int i = 1; i < size; i++) {
+		for (int i = 1; i < size; i++) {
 			key = array[i];
 			int j = i - 1;
-			while(j>=0 && key < array[j]) {
+			while (j >= 0 && key < array[j]) {
 				array[j + 1] = array[j];
 				j -= 1;
 			}
@@ -183,20 +468,22 @@ public class Sort {
 	}
 
 	/**
-	 * Sorting an array of Comparables according to the <i>Insertion-Sort algorithm</i>
-	 * after calling this function the given array will be sorted in ascending order
+	 * Sorting an array of Comparables according to the <i>Insertion-Sort
+	 * algorithm</i> after calling this function the given array will be sorted
+	 * in ascending order
 	 * 
-	 * @param array - int array to be sorted
+	 * @param array
+	 *            - int array to be sorted
 	 */
 	public static <T extends Comparable<T>> void insertionSort(T[] array) {
-		if(array.length <= 1)
+		if (array.length <= 1)
 			return;
 		int size = array.length;
 		T key;
-		for(int i = 1; i < size; i++) {
+		for (int i = 1; i < size; i++) {
 			key = array[i];
 			int j = i - 1;
-			while(j>=0 && key.compareTo(array[j]) == -1) {
+			while (j >= 0 && key.compareTo(array[j]) == -1) {
 				array[j + 1] = array[j];
 				j -= 1;
 			}
@@ -205,20 +492,22 @@ public class Sort {
 	}
 
 	/**
-	 * Sorting an array of integers according to the <i>Insertion-Sort algorithm</i>
-	 * after calling this function the given array will be sorted in ascending order
+	 * Sorting an array of integers according to the <i>Insertion-Sort
+	 * algorithm</i> after calling this function the given array will be sorted
+	 * in ascending order
 	 * 
-	 * @param array - int array to be sorted
+	 * @param array
+	 *            - int array to be sorted
 	 */
 	public static <T> void insertionSort(T[] array, Comparator<T> comperator) {
-		if(array.length <= 1)
+		if (array.length <= 1)
 			return;
 		int size = array.length;
 		T key;
-		for(int i = 1; i < size; i++) {
+		for (int i = 1; i < size; i++) {
 			key = array[i];
 			int j = i - 1;
-			while(j>=0 && comperator.compare(key, array[j]) == -1) {
+			while (j >= 0 && comperator.compare(key, array[j]) == -1) {
 				array[j + 1] = array[j];
 				j -= 1;
 			}
---
+++ Java/utility-belt/src/test/java/algorithms/TesterHelpers.java
@@ -0,0 +1,92 @@
+package algorithms;
+
+public class TesterHelpers {
+	public static class TestNumber {
+		private int i;
+		
+		public TestNumber(int i) {
+			this.i = i;
+		}
+		
+		public int getI() {
+			return i;
+		}
+		
+		@Override
+		public String toString() {
+			return "" + i;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + i;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			TestNumber other = (TestNumber) obj;
+			if (i != other.i)
+				return false;
+			return true;
+		}		
+	}
+
+	public static class ComparableNumber implements Comparable<ComparableNumber> {
+
+		private int i;
+		public ComparableNumber(int i) {
+			this.i = i;
+		}
+		
+		public int getI() {
+			return i;
+		}
+
+		@Override
+		public int compareTo(ComparableNumber o) {
+			if(i < o.i) {
+				return -1;
+			}
+			if(i > o.i) {
+				return 1;
+			}
+			return 0;
+		}
+
+		@Override
+		public String toString() {
+			return "" + i;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + i;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ComparableNumber other = (ComparableNumber) obj;
+			if (i != other.i)
+				return false;
+			return true;
+		}
+	}
+}
